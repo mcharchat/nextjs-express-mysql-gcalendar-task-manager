922354cf5e398557a282b060f85cd131d6921951
mcharchat <73533552+mcharchat@users.noreply.github.com>
2023-09-22 01:27:42 -0300

    added the needed scopes for calendar and email

--- lib/auth.ts
+++ lib/auth.ts
@@ -7,6 +7,12 @@ export const authOptions: NextAuthOptions = {
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+			authorization: {
+				params: {
+					scope:
+						"https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/gmail.insert https://www.googleapis.com/auth/gmail.send",
+				},
+			},
 		}),
 	],
 };
