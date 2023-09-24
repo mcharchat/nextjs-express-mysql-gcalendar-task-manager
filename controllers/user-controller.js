bce384016503ff48814435f52fba477c6f5373ad
mcharchat <73533552+mcharchat@users.noreply.github.com>
2023-09-24 01:33:35 -0300

    get user ready and updates the accessToken put user ready, only missing googleapi integration

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,13 +1,12 @@
 const { getToken } = require("next-auth/jwt");
-const { User } = require("../models");
+const { User, Squad } = require("../models");
 
 const userController = {
 	// GET users/me
 	getUsersMe: async (req, res) => {
 		//const secret = process.env.NEXTAUTH_SECRET;
 		const token = await getToken({ req });
-		//const accessToken = token['access-token'];
-		//console.log(accessToken);
+		const { accessToken, exp } = token;
 		// try to find the user in the database
 		const user = await User.findOne({
 			where: {
@@ -15,11 +14,78 @@ const userController = {
 			},
 			select: ["name", "email", "picture", "squadCode"],
 		});
+		if (user) {
+			await User.update(
+				{
+					accessToken,
+					exp,
+				},
+				{
+					where: {
+						email: token.email,
+					},
+				}
+			);
+		}
 
 		res.json(user);
 	},
 	// PUT users/me
 	putUsersMe: async (req, res) => {
+		const token = await getToken({ req });
+		const { name, email, picture, exp, accessToken } = token;
+		const { squadCode } = req.body;
+
+		// check if the squad exists
+		const squad = await Squad.findOne({
+			where: {
+				code: squadCode,
+			},
+		});
+		if (!squad) {
+			await Squad.create({
+				code: squadCode,
+			});
+		}
+		// check if the user exists
+		const user = await User.findOne({
+			where: {
+				email,
+			},
+		});
+		if (user) {
+			await User.update(
+				{
+					name,
+					email,
+					picture,
+					squadCode,
+					accessToken,
+					exp,
+				},
+				{
+					where: {
+						email,
+					},
+				}
+			);
+		} else {
+			await User.create({
+				name,
+				email,
+				picture,
+				squadCode,
+				accessToken,
+				exp,
+			});
+		}
+
+		res.json({
+			name,
+			email,
+			picture,
+			squadCode,
+		});
 	},
 };
 
